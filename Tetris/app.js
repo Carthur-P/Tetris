8de4827bc43c275de1529bbfd2bdddf27eedec23
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-06-08 22:46:48 +1200

    Adding div elements to array

--- Tetris/app.js
+++ Tetris/app.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const boardSize = 200;
+    const boardSize = 600;
     let board = document.getElementById('board');
     
     //Setting up the tetris grid
-    for (i = 0; i <= boardSize; i++){
+    for (i = 0; i < boardSize; i++){
         board.innerHTML += '<div></div>' 
     }
+
+    let squares = Array.from(document.querySelectorAll('#board div'));
+    console.log(squares);
 });
