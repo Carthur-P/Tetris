b8233aeaced621e7f4ccd717a8ec0472175c5f7f
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-06-10 09:50:59 +1200

    Adding L Tetriminoes

--- Tetris/app.js
+++ Tetris/app.js
@@ -19,4 +19,32 @@ document.addEventListener('DOMContentLoaded', () => {
       rotation4: [boardWidth+1, boardWidth*2+1, boardWidth*2+2, boardWidth*2+3]
     };
 
+    let IShape = {
+        rotation1: [1, boardWidth+2, boardWidth*2+2, boardWidth*3+2],
+        rotation2: [0, 1, 2, 3],
+        rotation1: [1, boardWidth+2, boardWidth*2+2, boardWidth*3+2],
+        rotation4: [0, 1, 2, 3]
+      };
+
+    //   let LShape = {
+    //     rotation1: [1, 2, boardWidth+2, boardWidth*2+2],
+    //     rotation2: [boardWidth+1, boardWidth+2, boardWidth+3, boardWidth*2+3],
+    //     rotation3: [1, boardWidth+2, boardWidth*2+2, boardWidth*2+1],
+    //     rotation4: [boardWidth+1, boardWidth*2+1, boardWidth*2+2, boardWidth*2+3]
+    //   };
+
+    //   let LShape = {
+    //     rotation1: [1, 2, boardWidth+2, boardWidth*2+2],
+    //     rotation2: [boardWidth+1, boardWidth+2, boardWidth+3, boardWidth*2+3],
+    //     rotation3: [1, boardWidth+2, boardWidth*2+2, boardWidth*2+1],
+    //     rotation4: [boardWidth+1, boardWidth*2+1, boardWidth*2+2, boardWidth*2+3]
+    //   };
+
+    //   let LShape = {
+    //     rotation1: [1, 2, boardWidth+2, boardWidth*2+2],
+    //     rotation2: [boardWidth+1, boardWidth+2, boardWidth+3, boardWidth*2+3],
+    //     rotation3: [1, boardWidth+2, boardWidth*2+2, boardWidth*2+1],
+    //     rotation4: [boardWidth+1, boardWidth*2+1, boardWidth*2+2, boardWidth*2+3]
+    //   };
+
 });
